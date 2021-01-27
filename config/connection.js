62a4548944f509e087a85f02d8391a0d5f38a377
Hao <hao09@yahoo.com>
2021-01-26 19:46:05 -0800

    deploy

--- config/connection.js
+++ config/connection.js
@@ -3,9 +3,9 @@ if (process.env.CLEARDB_DATABASE_URL) {
   connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
 } else {
   connection = mysql.createConnection({
-    host: "us-cdbr-east-03.cleardb.com",
-    user: "bfb4d7e6157abb",
-    password: "dc82952c",
+    host: "localhost",
+    user: "root",
+    password: "password",
     database: "heroku_635e6d7eed5b116"
   });
 }
